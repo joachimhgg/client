0a858ae1e6db16f6be7642e02e180cb1ab1ad1d2
dzier <dzier@nvidia.com>
2022-08-19 16:11:37 +0000

    Add test helper methods

--- src/c++/perf_analyzer/test_command_line_parser.cc
+++ src/c++/perf_analyzer/test_command_line_parser.cc
@@ -38,6 +38,122 @@ CHECK_STRING(const char* name, std::string str, const char* val)
       !str.compare(val), name, " expecting '", val, "', found '", str, "'");
 }
 
+inline void
+CHECK_STRING(std::string act, std::string exp)
+{
+  CHECK_MESSAGE(
+      !act.compare(exp), "Expecting: '", exp, "', Found: '", act, "'");
+}
+
+// Performs a doc test check against all the individual parameters
+// in a PAParams object.
+//
+// /param act actual object under test
+// /param exp expected value for object
+//
+inline void
+CHECK_PARAMS(PAParamsPtr act, PAParamsPtr exp)
+{
+  CHECK(act->verbose == exp->verbose);
+  CHECK(act->streaming == exp->streaming);
+  CHECK(act->extra_verbose == exp->extra_verbose);
+  CHECK(act->max_threads == exp->max_threads);
+  CHECK(act->max_threads_specified == exp->max_threads_specified);
+  CHECK(act->sequence_length == exp->sequence_length);
+  CHECK(act->percentile == exp->percentile);
+  CHECK(act->user_data.size() == exp->user_data.size());
+  CHECK(act->input_shapes.size() == exp->input_shapes.size());
+  CHECK(act->measurement_window_ms == exp->measurement_window_ms);
+  CHECK(act->using_concurrency_range == exp->using_concurrency_range);
+  CHECK(act->concurrency_range[0] == exp->concurrency_range[0]);
+  CHECK(act->concurrency_range[1] == exp->concurrency_range[1]);
+  CHECK(act->concurrency_range[2] == exp->concurrency_range[2]);
+  CHECK(act->latency_threshold_ms == exp->latency_threshold_ms);
+  CHECK(act->stability_threshold == doctest::Approx(act->stability_threshold));
+  CHECK(act->max_trials == exp->max_trials);
+  CHECK(act->zero_input == exp->zero_input);
+  CHECK(act->string_length == exp->string_length);
+  CHECK_STRING(act->string_data, exp->string_data);
+  CHECK(act->async == exp->async);
+  CHECK(act->forced_sync == exp->forced_sync);
+  CHECK(act->using_request_rate_range == exp->using_request_rate_range);
+  CHECK(
+      act->request_rate_range[0] ==
+      doctest::Approx(exp->request_rate_range[0]));
+  CHECK(
+      act->request_rate_range[1] ==
+      doctest::Approx(exp->request_rate_range[1]));
+  CHECK(
+      act->request_rate_range[2] ==
+      doctest::Approx(exp->request_rate_range[2]));
+  CHECK(act->num_of_sequences == exp->num_of_sequences);
+  CHECK(act->search_mode == exp->search_mode);
+  CHECK(act->request_distribution == exp->request_distribution);
+  CHECK(act->using_custom_intervals == exp->using_custom_intervals);
+  CHECK_STRING(act->request_intervals_file, exp->request_intervals_file);
+  CHECK(act->shared_memory_type == exp->shared_memory_type);
+  CHECK(act->output_shm_size == exp->output_shm_size);
+  CHECK(act->kind == exp->kind);
+  CHECK_STRING(act->model_signature_name, exp->model_signature_name);
+  CHECK(act->using_grpc_compression == exp->using_grpc_compression);
+  CHECK(act->compression_algorithm == exp->compression_algorithm);
+  CHECK(act->measurement_mode == exp->measurement_mode);
+  CHECK(act->measurement_request_count == exp->measurement_request_count);
+  CHECK_STRING(act->triton_server_path, exp->triton_server_path);
+  CHECK_STRING(act->model_repository_path, exp->model_repository_path);
+  CHECK(act->start_sequence_id == exp->start_sequence_id);
+  CHECK(act->sequence_id_range == exp->sequence_id_range);
+  CHECK_STRING(
+      act->ssl_options.ssl_grpc_certificate_chain_file,
+      exp->ssl_options.ssl_grpc_certificate_chain_file);
+  CHECK_STRING(
+      act->ssl_options.ssl_grpc_private_key_file,
+      exp->ssl_options.ssl_grpc_private_key_file);
+  CHECK_STRING(
+      act->ssl_options.ssl_grpc_root_certifications_file,
+      exp->ssl_options.ssl_grpc_root_certifications_file);
+  CHECK(act->ssl_options.ssl_grpc_use_ssl == exp->ssl_options.ssl_grpc_use_ssl);
+  CHECK_STRING(
+      act->ssl_options.ssl_https_ca_certificates_file,
+      exp->ssl_options.ssl_https_ca_certificates_file);
+  CHECK_STRING(
+      act->ssl_options.ssl_https_client_certificate_file,
+      exp->ssl_options.ssl_https_client_certificate_file);
+  CHECK_STRING(
+      act->ssl_options.ssl_https_client_certificate_type,
+      exp->ssl_options.ssl_https_client_certificate_type);
+  CHECK_STRING(
+      act->ssl_options.ssl_https_private_key_file,
+      exp->ssl_options.ssl_https_private_key_file);
+  CHECK_STRING(
+      act->ssl_options.ssl_https_private_key_type,
+      exp->ssl_options.ssl_https_private_key_type);
+  CHECK(
+      act->ssl_options.ssl_https_verify_host ==
+      exp->ssl_options.ssl_https_verify_host);
+  CHECK(
+      act->ssl_options.ssl_https_verify_peer ==
+      exp->ssl_options.ssl_https_verify_peer);
+  CHECK(act->verbose_csv == exp->verbose_csv);
+  CHECK(act->enable_mpi == exp->enable_mpi);
+  CHECK(act->trace_options.size() == exp->trace_options.size());
+  CHECK(act->using_old_options == exp->using_old_options);
+  CHECK(act->dynamic_concurrency_mode == exp->dynamic_concurrency_mode);
+  CHECK(act->url_specified == exp->url_specified);
+  CHECK_STRING(act->url, exp->url);
+  CHECK_STRING(act->model_name, exp->model_name);
+  CHECK_STRING(act->model_version, exp->model_version);
+  CHECK(act->batch_size == exp->batch_size);
+  CHECK(act->using_batch_size == exp->using_batch_size);
+  CHECK(act->concurrent_request_count == exp->concurrent_request_count);
+  CHECK(act->protocol == exp->protocol);
+  CHECK(act->http_headers->size() == exp->http_headers->size());
+  CHECK(act->max_concurrency == exp->max_concurrency);
+  CHECK_STRING(act->filename, act->filename);
+  CHECK(act->mpi_driver != nullptr);
+  CHECK_STRING(act->memory_type, exp->memory_type);
+}
+
 TEST_CASE("Testing PerfAnalyzerParameters")
 {
   PAParamsPtr params(new PerfAnalyzerParameters{});
@@ -49,8 +165,6 @@ TEST_CASE("Testing PerfAnalyzerParameters")
   CHECK(params->max_threads_specified == false);
   CHECK(params->sequence_length == 20);
   CHECK(params->percentile == -1);
-  CHECK(params->user_data.size() == 0);
-
   CHECK(params->user_data.size() == 0);
   CHECK(params->input_shapes.size() == 0);
   CHECK(params->measurement_window_ms == 5000);
@@ -137,20 +251,46 @@ TEST_CASE("Testing PerfAnalyzerParameters")
   CHECK_STRING("memory_type", params->memory_type, "system");
 }
 
+// Test CLParser Class that captures the usage string but suppresses the output
+//
+class TestCLParser : public CLParser {
+ public:
+  std::string get_usage_message() const { return usage_message_; }
+
+ private:
+  std::string usage_message_;
+
+  virtual void usage(const std::string& msg = std::string())
+  {
+    usage_message_ = msg;
+  }
+};
+
 TEST_CASE("Testing Command Line Parser")
 {
-  CLParser parser;
+  TestCLParser parser;
+
+  SUBCASE("with no parameters")
+  {
+    char* argv[1] = {"test_perf_analyzer"};
+    int argc = 1;
+    PAParamsPtr p;
+    REQUIRE_NOTHROW(p = parser.parse(argc, argv));
+
+    CHECK_STRING(
+        "Usage Message", parser.get_usage_message(),
+        "-m flag must be specified");
+  }
 
   SUBCASE("with min parameters")
   {
     char* argv[3] = {"test_perf_analyzer", "-m", "my_model"};
     int argc = 3;
-    PAParamsPtr p = parser.parse(argc, argv);
+    PAParamsPtr act = parser.parse(argc, argv);
 
-    CHECK(p->verbose == false);
-    CHECK(p->streaming == false);
-    CHECK(p->extra_verbose == false);
-    CHECK_STRING("model_name", p->model_name, "my_model");
+    PAParamsPtr exp{new PerfAnalyzerParameters()};
+    exp->model_name = "should fail";
+    CHECK_PARAMS(act, exp);
   }
 }
 
